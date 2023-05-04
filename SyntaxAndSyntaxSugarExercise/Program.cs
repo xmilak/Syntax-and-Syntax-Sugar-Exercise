d05f614d3040d6023fb5f3299b6cc35595f42092
xmilak <126503532+xmilak@users.noreply.github.com>
2023-05-03 21:12:23 -0400

    Done

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace SyntaxAndSyntaxSugarExercise
+﻿using System.IO.Pipes;
+
+namespace SyntaxAndSyntaxSugarExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Initializing 
+            Console.WriteLine("Is your number 9?");
+            var answer = 4;
+
+            // complete Inferred typing, String Interpolation, Ternary Operator
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+
+            Console.WriteLine(response);
         }
     }
 }
